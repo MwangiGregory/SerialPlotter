592c4efa28ef821139e57e73a7a65773c1c11b6d
MwangiGregory <gmwaburi@gmail.com>
2024-09-29 01:19:45 +0300

    Implement autoscrolling logging window
    This window displays new text and autoscrolls with the text. This is the
    first step in implement a serial monitor that will receive serial text
    from a microcontroller for debug display purposes.

--- src/SerialApp.cpp
+++ src/SerialApp.cpp
@@ -1,11 +1,17 @@
 #include "Application.h"
 
-#define WIN_WIDTH 640
-#define WIN_HEIGHT 480
+#define WIN_WIDTH 1280
+#define WIN_HEIGHT 720
 #define APP_TITLE "Serial Plotter"
 
 class SerialApplication : public Application
 {
+private:
+    bool serial_plotter_close = false;
+    bool serial_monitor_close = false;
+    ImGuiTextBuffer Buf;
+    bool AutoScroll = true;
+
 public:
     SerialApplication(int width, int height, const char *title)
         : Application(width, height, title)
@@ -17,11 +23,69 @@ public:
 
     void Update()
     {
-        bool show_demo_window = true;
+        static unsigned long int count = 0;
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        ImGui::Begin("Overall");
+        ImGui::ShowDemoWindow();
+        SerialPlotter();
+
+        // Serial Monitor text
+        count++;
+        Buf.appendf("[Count var]: %lu\n", count);
+
+        SerialMonitor();
+
+        ImGui::End();
+    }
+
+    void SerialPlotter()
+    {
+        if (ImGui::Begin("Serial SerialPlotter", &serial_plotter_close))
+        {
+            ImGui::Text("I am an awesome Serial Plotter");
+        }
+        ImGui::End();
+    }
+
+    void SerialMonitor()
+    {
+        if (ImGui::Begin("Serial Monitor", &serial_monitor_close))
+        {
+            // Options menu
+            if (ImGui::BeginPopup("Options"))
+            {
+                ImGui::Checkbox("Auto-scroll", &AutoScroll);
+                ImGui::EndPopup();
+            }
+
+            // Main window
+            if (ImGui::Button("Options"))
+                ImGui::OpenPopup("Options");
+            ImGui::SameLine();
+            bool clear = ImGui::Button("Clear");
+            ImGui::SameLine();
+            bool copy = ImGui::Button("Copy");
+
+            ImGui::Separator();
+
+            if (ImGui::BeginChild("scrolling", ImVec2(0.0, 0.0), ImGuiChildFlags_None, ImGuiWindowFlags_HorizontalScrollbar))
+            {
+                if (clear)
+                    Buf.clear();
+                if (copy)
+                    ImGui::LogToClipboard();
+
+                const char *buf_begin = Buf.begin();
+                const char *buf_end = Buf.end();
+
+                ImGui::TextUnformatted(buf_begin, buf_end);
+
+                if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+                    ImGui::SetScrollHereY(1.0);
+            }
+            ImGui::EndChild();
+        }
+        ImGui::End();
     }
 };
 
