610584bac4c1baaadcf1f9799e392302236183b0
MwangiGregory <gmwaburi@gmail.com>
2024-10-02 13:32:01 +0300

    Implement serial communication configuration controls
     - This commit implements widgets used to configure a serial communication.
     - Adds logic to DetectSerialPorts function to prevent it from closing
       an already existing serial communication linl

--- src/SerialApp.cpp
+++ src/SerialApp.cpp
@@ -11,12 +11,20 @@ class SerialApplication : public Application
 {
 private:
     char buffer[1024] = {0};
-    const unsigned int baud_rate = 500000;
-    const char *serial_port_name = "/dev/ttyUSB0";
+
     serialib serial;
     std::vector<std::string> ports;
 
-    int selected_port_idx = 0;
+    std::string serial_port_name;
+    int selected_port_idx = -1;
+
+    const unsigned int baud_rate = 0;
+    const int rates[28] = {110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400,
+                           56000, 57600, 115200, 128000, 256000, 230400, 460800, 500000, 576000, 921600,
+                           1000000, 1152000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000};
+    int selected_baud_rate_idx = -1;
+
+    bool start_monitor = false;
 
     bool serial_plotter_close = false;
     bool serial_monitor_close = false;
@@ -31,13 +39,6 @@ public:
 
     void Setup()
     {
-        if (serial.openDevice(serial_port_name, baud_rate) != 1)
-        {
-            printf("Failed to connect to %s\n", serial_port_name);
-            std::exit(EXIT_FAILURE);
-        }
-        printf("Successful connection to %s\n", serial_port_name);
-
         ports = DetectSerialPorts();
     }
 
@@ -48,27 +49,36 @@ public:
 
     void Update()
     {
+        ports = DetectSerialPorts();
+
         UpdateSerialMonitor();
         SerialMonitor();
 
         SerialPlotter();
+        DebugWindow();
     }
 
     void UpdateSerialMonitor()
     {
         // Serial Monitor text
-        if (serial.isDeviceOpen() && serial.available() > 0)
+        if (start_monitor && serial.isDeviceOpen() && serial.available() > 0)
         {
             serial.readString(buffer, '\n', 1024);
             Buf.appendf("[Serial]: %s\n", buffer);
         }
-        else if (!serial.isDeviceOpen())
-        {
+        else if (start_monitor == false)
             serial.closeDevice();
-            if (serial.openDevice(serial_port_name, baud_rate) != 1)
-                Buf.appendf("Serial device is not open. Retrying at %s\n", serial_port_name);
-            else
-                Buf.appendf("Successful connection to %s\n", serial_port_name);
+        else if (!serial.isDeviceOpen() && start_monitor)
+        {
+            // serial.closeDevice();
+            if (selected_port_idx > -1 && selected_baud_rate_idx > 0) // Check for a selected item
+            {
+                serial_port_name = ports[selected_port_idx];
+                if (serial.openDevice(serial_port_name.c_str(), rates[selected_baud_rate_idx]) != 1)
+                    Buf.appendf("Serial device is not open. Retrying at %s\n", serial_port_name.c_str());
+                else
+                    Buf.appendf("Successful connection to %s\n", serial_port_name.c_str());
+            }
         }
     }
 
@@ -86,6 +96,16 @@ public:
         ImGui::End();
     }
 
+    void DebugWindow()
+    {
+
+        if (ImGui::Begin("Debug Window"))
+        {
+            ImGui::Text("selected_port_idx: %d\n", selected_port_idx);
+        }
+        ImGui::End();
+    }
+
     void SerialMonitor()
     {
         if (ImGui::Begin("Serial Monitor", &serial_monitor_close))
@@ -97,34 +117,61 @@ public:
                 ImGui::EndPopup();
             }
 
-            // Main window
+            if (ImGui::TreeNode("Select a Serial Port"))
+            {
+                if (ImGui::BeginListBox(""))
+                {
+                    for (int n = 0; n < (int)ports.size(); n++)
+                    {
+                        const bool is_selected = (selected_port_idx == n);
+                        if (ImGui::Selectable(ports[n].c_str(), is_selected))
+                            selected_port_idx = n;
+                    }
+                    ImGui::EndListBox();
+                }
+                ImGui::TreePop();
+            }
+
+            ImGui::SameLine();
+            if (ImGui::TreeNode("Select a Baud Rate"))
+            {
+                if (ImGui::BeginListBox(""))
+                {
+                    for (int n = 0; n < IM_ARRAYSIZE(rates); n++)
+                    {
+                        const bool is_selected = (selected_baud_rate_idx == n);
+                        if (ImGui::Selectable(std::to_string(rates[n]).c_str(), is_selected))
+                            selected_baud_rate_idx = n;
+                    }
+                    ImGui::EndListBox();
+                }
+                ImGui::TreePop();
+            }
+
+            ImGui::Separator();
+
+            ImGui::Text("Connected to: %40s", serial_port_name.c_str());
+            ImGui::SameLine();
+            static bool start_button_show = true;
+
+            if (start_button_show)
+                if (ImGui::Button("Start Monitor"))
+                    start_monitor = true;
+
+            start_button_show = start_monitor ? false : true;
+
+            if (start_button_show == false)
+                if (ImGui::Button("Stop Monitor"))
+                    start_monitor = false;
+
+            ImGui::Separator();
+
             if (ImGui::Button("Options"))
                 ImGui::OpenPopup("Options");
             ImGui::SameLine();
             bool clear = ImGui::Button("Clear");
             ImGui::SameLine();
             bool copy = ImGui::Button("Copy");
-
-            ImGui::SameLine();
-            if (ImGui::TreeNode("Serial Ports"))
-            {
-                if (ImGui::BeginListBox("Serial Ports"))
-                {
-                    for (int n = 0; n < (int)ports.size(); n++)
-                    {
-                        const bool is_selected = (selected_port_idx == n);
-
-                        if (ImGui::Selectable(ports[n].c_str(), is_selected))
-                            selected_port_idx = n;
-
-                        // // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-                        // if (is_selected)
-                        //     ImGui::SetItemDefaultFocus();
-                    }
-                    ImGui::EndListBox();
-                }
-                ImGui::TreePop();
-            }
 
             ImGui::Separator();
 
@@ -151,77 +198,100 @@ public:
 
     std::vector<std::string> DetectSerialPorts()
     {
-#define BUF_SIZE 64
-        char port_name[BUF_SIZE] = {0};
+        serialib tester;
+        int baud = 115200;
+        char port_name[64];
+
         std::vector<std::string> port_names;
 
         for (int i = 1; i < 100; i++)
         {
-// Prepare the port name (Windows)
+// Prepare the port name (Windows)`
 #if defined(_WIN32) || defined(_WIN64)
             sprintf(port_name, "\\\\.\\COM%d", i);
-            if (serial.openDevice(port_name, baud_rate) == 1)
+            if (serial_port_name == std::string(port_name))
             {
                 port_names.push_back(port_name);
-                serial.closeDevice();
+                continue;
+            }
+            else if (tester.openDevice(port_name, baud) == 1)
+            {
+                port_names.push_back(port_name);
+                tester.closeDevice();
             }
 #endif
 
 // Prepare the port names (Linux)
 #ifdef __linux__
-            // Check for /dev/ttyS*
-            // sprintf(port_name, "/dev/ttyS%d", i - 1);
-            // if (serial.openDevice(port_name, baud_rate) == 1)
-            // {
-            //     port_names.push_back(port_name);
-            //     serial.closeDevice();
-            // }
 
             // Check for /dev/ttyUSB*
             sprintf(port_name, "/dev/ttyUSB%d", i - 1);
-            if (serial.openDevice(port_name, baud_rate) == 1)
+            if (serial_port_name == std::string(port_name))
             {
                 port_names.push_back(port_name);
-                serial.closeDevice();
+                continue;
+            }
+            else if (tester.openDevice(port_name, baud) == 1)
+            {
+                port_names.push_back(port_name);
+                tester.closeDevice();
             }
 
             // Check for /dev/ttyACM*
             sprintf(port_name, "/dev/ttyACM%d", i - 1);
-            if (serial.openDevice(port_name, baud_rate) == 1)
+            if (serial_port_name == std::string(port_name))
             {
                 port_names.push_back(port_name);
-                serial.closeDevice();
+                continue;
+            }
+            else if (tester.openDevice(port_name, baud) == 1)
+            {
+                port_names.push_back(port_name);
+                tester.closeDevice();
             }
 
             // Check for /dev/ttyAMA*
             sprintf(port_name, "/dev/ttyAMA%d", i - 1);
-            if (serial.openDevice(port_name, baud_rate) == 1)
+            if (serial_port_name == std::string(port_name))
             {
                 port_names.push_back(port_name);
-                serial.closeDevice();
+                continue;
+            }
+            else if (tester.openDevice(port_name, baud) == 1)
+            {
+                port_names.push_back(port_name);
+                tester.closeDevice();
             }
 #endif
             // Prepare the port names (macOS)
 #ifdef __APPLE__
             // Check for /dev/tty.*
             sprintf(device_name, "/dev/tty.%d", i);
-            if (device.openDevice(device_name, 115200) == 1)
+            if (serial_port_name == std::string(port_name))
             {
-                port_names.push_back(device_name);
-                device.closeDevice();
+                port_names.push_back(port_name);
+                continue;
+            }
+            else if (tester.openDevice(port_name, baud) == 1)
+            {
+                port_names.push_back(port_name);
+                tester.closeDevice();
             }
 
             // Check for /dev/cu.*
             sprintf(device_name, "/dev/cu.%d", i);
-            if (device.openDevice(device_name, 115200) == 1)
+            if (serial_port_name == std::string(port_name))
             {
-                port_names.push_back(device_name);
-                device.closeDevice();
+                port_names.push_back(port_name);
+                continue;
+            }
+            else if (tester.openDevice(port_name, baud) == 1)
+            {
+                port_names.push_back(port_name);
+                tester.closeDevice();
             }
 #endif
-
         } /* end of for loop */
-
         return port_names;
     }
 };
